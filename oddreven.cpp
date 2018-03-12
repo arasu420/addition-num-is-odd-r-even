36a07039d43e7596202a88280a33515b92afa5cd
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-12 17:11:04 +0530

    Create oddreven.cpp

---
+++ oddreven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int a,b,res;
+cout<<"enter the numbers";
+cin>>a>>b;
+res=a+b;
+if(res%2==0)
+{
+    cout<<"even";
+}
+else
+{
+    cout<<"odd";
+}
+  return 0;
+}
